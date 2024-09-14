b473e239a306bc61062ac54ff79831867c52a3cb
RajKKapadia <raajforyou@gmail.com>
2024-09-14 21:54:48 +0530

    added try catch in the send fb message function

--- fb_graph_api.py
+++ fb_graph_api.py
@@ -4,14 +4,17 @@ from mongodb_api import get_page_token
 
 
 def send_message_to_fb_messenger(recipient_id: str, message_text: str, page_id: str) -> None:
-    page_token = get_page_token(page_id=page_id)
-    url = f"""https://graph.facebook.com/v17.0/me/messages?access_token={page_token["token"]}"""
-    data = {
-        "recipient": {"id": recipient_id},
-        "message": {"text": message_text}
-    }
-    response = requests.post(url, json=data)
-    if response.status_code == 200:
-        print("FB MESSENGER MESSAGE SENT SUCCESSFULLY.")
-    else:
+    try:
+        page_token = get_page_token(page_id=page_id)
+        url = f"""https://graph.facebook.com/v17.0/me/messages?access_token={page_token["token"]}"""
+        data = {
+            "recipient": {"id": recipient_id},
+            "message": {"text": message_text}
+        }
+        response = requests.post(url, json=data)
+        if response.status_code == 200:
+            print("FB MESSENGER MESSAGE SENT SUCCESSFULLY.")
+        else:
+            print("FB MESSENGER MESSAGE SENT FAILED.")
+    except:
         print("FB MESSENGER MESSAGE SENT FAILED.")
